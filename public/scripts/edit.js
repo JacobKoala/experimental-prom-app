5eed1bf39a3a4386be89db36e4d771d8387bf35c
JacobKoala <42662002+JacobKoala@users.noreply.github.com>
2019-04-03 14:13:48 -0500

    Data can now be sent! 🦀🦀🦀

--- public/scripts/edit.js
+++
@@ -1,2 +0,0 @@
-alert("This is JavaScript...");
-createStudent(068643, "Student Name", "", "L");
--- public/scripts/functions.js
+++ public/scripts/functions.js
@@ -8,3 +8,11 @@ function createStudent(studentID, studentName, idPhoto, typedShirtSize) {
 		console.error('Error when adding student', error)
 	});
 }
+
+function submit() {
+	// example: createStudent(068643, "Student Name", "", "L");
+	var nameInput = document.getElementById("nameInput").value;
+	var shirtInput = document.getElementById("shirtInput").value;
+	var ticketInput = document.getElementById("ticketInput").value;
+	createStudent(000000, nameInput, "", shirtInput);
+}
