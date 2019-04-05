4b7103c66e61bf4e93a8c5765a2241afabcf8600
JacobKoala <42662002+JacobKoala@users.noreply.github.com>
2019-04-05 14:01:52 -0500

    Update functions.js

--- public/scripts/functions.js
+++ public/scripts/functions.js
@@ -1,9 +1,11 @@
 // This adds a new student to the database.
-function createStudent(studentID, studentName, idPhoto, typedShirtSize) {
+function createStudent(studentID, studentName, idPhoto, typedShirtSize, ticketNumber) {
 	return firebase.firestore().collection('students').add({
 		name: studentName,
+		ticketNumber: ticketNumber,
 		photoID: idPhoto,
-		shirtSize: typedShirtSize // todo: add timestamp on next line
+		shirtSize: typedShirtSize,
+		timestamp: Date.now()
 	}).catch(function(error) {
 		console.error('Error when adding student', error)
 	});
@@ -13,5 +15,8 @@ function submit() {
 	var nameInput = document.getElementById("nameInput").value;
 	var shirtInput = document.getElementById("shirtInput").value;
 	var ticketInput = document.getElementById("ticketInput").value;
-	createStudent(000000, nameInput, "", shirtInput);
+	createStudent(000000, nameInput, "", shirtInput, ticketInput);
+	document.getElementById("nameInput").value = "";
+	document.getElementById("shirtInput").value = "";
+	document.getElementById("ticketInput").value = "";
 }
