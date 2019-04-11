e100fe14442aff3964a404328b75b7ac84cfc017
JacobKoala <42662002+JacobKoala@users.noreply.github.com>
2019-04-11 13:56:06 -0500

    Fix Import
    In this commit I replaced external URLs with internal URLs, added links to relevant CSS, and added links to Firebase scripts.

--- Programs/FaR URLs.bat
+++ Programs/FaR URLs.bat
@@ -1,5 +1,7 @@
 FART -r "..\public\*.html" "https://codepen.io/Kyleufree/full/JxeXZa" "https://experimental-prom-app.firebaseapp.com/main.html"
 FART -r "..\public\*.html" "https://codepen.io/DonkeyKong246/full/vbrVyL" "https://experimental-prom-app.firebaseapp.com/edit.html"
+FART -r "..\public\*.html" "https://codepen.io/DonkeyKong246/full/pYyzqB" "https://experimental-prom-app.firebaseapp.com/database.html"
+FART -r "..\public\*.html" "https://codepen.io/DonkeyKong246/pen/JzeLBN" "https://experimental-prom-app.firebaseapp.com/outOfSchool.html"
 FART -r "..\public\*.html" "<!-- main.css -->" "<link> <!-- link to CSS -->"
 FART -r "..\public\*.html" "<!-- edit.css -->" "<link> <!-- link to CSS -->"
 cmd /k
