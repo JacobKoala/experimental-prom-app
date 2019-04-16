aca43fb094cc1bd0d62f9d5b352b9ce4d10584ef
JacobKoala <42662002+JacobKoala@users.noreply.github.com>
2019-04-16 13:54:05 -0500

    Update FaR URLs.bat

--- Programs/FaR URLs.bat
+++ Programs/FaR URLs.bat
@@ -1,4 +1,4 @@
-FART -r "..\public\*.html" "https://codepen.io/Kyleufree/full/JxeXZa" "https://experimental-prom-app.firebaseapp.com/main.html"
+FART -r "..\public\*.html" "https://codepen.io/Kyleufree/full/JxeXZa" "https://experimental-prom-app.firebaseapp.com"
 FART -r "..\public\*.html" "https://codepen.io/DonkeyKong246/full/vbrVyL" "https://experimental-prom-app.firebaseapp.com/edit.html"
 FART -r "..\public\*.html" "https://codepen.io/DonkeyKong246/full/pYyzqB" "https://experimental-prom-app.firebaseapp.com/database.html"
 FART -r "..\public\*.html" "https://codepen.io/DonkeyKong246/pen/JzeLBN" "https://experimental-prom-app.firebaseapp.com/outOfSchool.html"
