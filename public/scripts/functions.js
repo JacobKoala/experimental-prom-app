4f6d4e84df756ff5fd3bb57539b62885f0b0bf10
JacobKoala <42662002+JacobKoala@users.noreply.github.com>
2019-04-16 14:02:53 -0500

    Implement display of data

--- public/scripts/functions.js
+++ public/scripts/functions.js
@@ -18,6 +18,7 @@ function getStudentByTicket(ticketNumber) {
         querySnapshot.forEach(function(doc) {
             // doc.data() is never undefined for query doc snapshots
             console.log(doc.id, " => ", doc.data());
+						document.getElemendById("databaseOutput") = doc.data();
         });
     })
     .catch(function(error) {
