3621f4ebf3095763f8fadfc7372140cc39080729
JacobKoala <42662002+JacobKoala@users.noreply.github.com>
2019-04-16 14:26:52 -0500

    Revert "Implemented display infrastructutre"
    This reverts commit decdabcbe37509fe50bd080960cc60179cb3928d.

--- public/scripts/functions.js
+++ public/scripts/functions.js
@@ -18,7 +18,8 @@ function getStudentByTicket(ticketNumber) {
         querySnapshot.forEach(function(doc) {
             // doc.data() is never undefined for query doc snapshots
             console.log(doc.id, " => ", doc.data());
-						w3.displayObject("databaseOutput", doc.data());
+						document.getElementById("databaseOutput").innerHTML = doc.data();
+						window.alert(doc.data());
         });
     })
     .catch(function(error) {
