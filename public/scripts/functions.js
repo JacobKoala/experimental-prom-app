755d8a936d1be4df332d6b8100689af211dafdf3
JacobKoala <42662002+JacobKoala@users.noreply.github.com>
2019-04-17 13:43:45 -0500

    Update functions.js

--- public/scripts/functions.js
+++ public/scripts/functions.js
@@ -18,8 +18,8 @@ function getStudentByTicket(ticketNumber) {
         querySnapshot.forEach(function(doc) {
             // doc.data() is never undefined for query doc snapshots
             console.log(doc.id, " => ", doc.data());
-						document.getElementById("databaseOutput").innerHTML = doc.data();
-						window.alert(doc.data());
+						document.getElementById("databaseOutput").innerHTML = doc.data().name;
+						window.alert(doc.data().name);
         });
     })
     .catch(function(error) {
