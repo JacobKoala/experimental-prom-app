804683be15e8e2d229f471c3aa57cc3456d104bf
JacobKoala <42662002+JacobKoala@users.noreply.github.com>
2019-04-17 14:16:00 -0500

    Split name into first name and last name

--- public/scripts/functions.js
+++ public/scripts/functions.js
@@ -1,7 +1,9 @@
 // This adds a new student to the database.
-function createStudent(studentID, studentName, idPhoto, typedShirtSize, ticketNumber) {
+function createStudent(studentID, firstName, lastName, idPhoto, typedShirtSize, ticketNumber) {
 	return firebase.firestore().collection('students').add({
-		name: studentName,
+		name: firstName + " " + lastName,
+		firstName: firstName,
+		lastName: lastName,
 		ticketNumber: ticketNumber,
 		photoID: idPhoto,
 		shirtSize: typedShirtSize,
@@ -30,11 +32,13 @@ function getStudentByTicket(ticketNumber) {
 }
 
 function submit() {
-	var nameInput = document.getElementById("nameInput").value;
+	var firstInput = document.getElementById("firstInput").value;
+	var lastInput = document.getElementById("lastInput").value;
 	var shirtInput = document.getElementById("shirtInput").value;
 	var ticketInput = document.getElementById("ticketInput").value;
-	createStudent(000000, nameInput, 0, shirtInput, parseInt(ticketInput, 10));
-	document.getElementById("nameInput").value = "";
+	createStudent(000000, firstInput, lastInput, 0, shirtInput, parseInt(ticketInput, 10));
+	document.getElementById("firstInput").value = "";
+	document.getElementById("lastInput").value = "";
 	document.getElementById("shirtInput").value = "";
 	document.getElementById("ticketInput").value = "";
 }
