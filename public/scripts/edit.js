c92dd935bf351a99c3fe2307962a2ea7b8b43bfd
JacobKoala <42662002+JacobKoala@users.noreply.github.com>
2019-04-25 13:59:41 -0500

    Add photo input

--- public/scripts/edit.js
+++ public/scripts/edit.js
@@ -1,3 +1,7 @@
+function doSomethingWithFiles() {
+  console.log(fileInput);
+}
+
 window.onload = function() {
   document.getElementById('ticketInput').onkeydown = function(event) {
       if (event.keyCode == 13) {
