6aefaffc909e2799ad8e0b9cc429ccf90cdebb62
JacobKoala <42662002+JacobKoala@users.noreply.github.com>
2019-04-29 00:38:22 -0500

    Make camera work properly
    I commented out the manual upload file option because it doesn't work. At the moment, the images cannot be retrieved using the website yet...

--- public/scripts/edit.js
+++ public/scripts/edit.js
@@ -45,12 +45,18 @@ function saveImage() {
             console.log(doc.id, " => ", doc.data());
 						currentStudentId = doc.id;
         });
+				console.log(currentStudentId);
+				console.log(currentStudentTicketNumber);
+				var studentRef = storageRef.child(currentStudentTicketNumber + ".jpg");
+				var file = document.getElementById('photo').src;
+				console.log(file);
+				var uploadTask = studentRef.putString(file, 'data_url').then(function(snapshot) {
+					console.log('Uploaded a data_url string!');
+				});
     })
     .catch(function(error) {
         console.log("Error getting documents: ", error);
     });
-	var studentRef = storageRef.child(currentStudentId + ".jpg");
-	var uploadTask = studentRef.put(fileInput);
 }
 
 window.onload = function() {
@@ -59,35 +65,4 @@ window.onload = function() {
           submit();
       }
   }
-  // Grab elements, create settings, etc.
-	var video = document.getElementById('video');
-
-	// Get access to the camera!
-	if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-	    // Not adding `{ audio: true }` since we only want video now
-	    navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
-	        //video.src = window.URL.createObjectURL(stream);
-	        video.srcObject = stream;
-	        video.play();
-	    });
-	}
-
-	/* Legacy code below: getUserMedia
-	else if(navigator.getUserMedia) { // Standard
-	    navigator.getUserMedia({ video: true }, function(stream) {
-	        video.src = stream;
-	        video.play();
-	    }, errBack);
-	} else if(navigator.webkitGetUserMedia) { // WebKit-prefixed
-	    navigator.webkitGetUserMedia({ video: true }, function(stream){
-	        video.src = window.webkitURL.createObjectURL(stream);
-	        video.play();
-	    }, errBack);
-	} else if(navigator.mozGetUserMedia) { // Mozilla-prefixed
-	    navigator.mozGetUserMedia({ video: true }, function(stream){
-	        video.srcObject = stream;
-	        video.play();
-	    }, errBack);
-	}
-	*/
 }
