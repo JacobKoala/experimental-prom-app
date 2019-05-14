4f2537001488773db3a17b98fc6a9a63a80145a0
JacobKoala <42662002+JacobKoala@users.noreply.github.com>
2019-05-14 09:34:44 -0500

    Clean up database.js

--- public/scripts/database.js
+++ public/scripts/database.js
@@ -76,19 +76,23 @@ function loadImage() {
 	  img.src = url;
 		document.getElementById("ticketInput").value = "";
 	}).catch(function(error) {
-	  // Handle any errors
-		storageRef.child(parseInt(document.getElementById("ticketInput").value, 10) + ".jpg").getDownloadURL().then(function(url) {
-		  // `url` is the download URL for 'images/stars.jpg'
+		photoWithoutZero();
+	});
+}
 
-		  // Or inserted into an <img> element:
-			console.log(url);
-		  var img = document.getElementById('imageSpace');
-		  img.src = url;
-			document.getElementById("ticketInput").value = "";
-		}).catch(function(error) {
-		  // Handle any errors
-			alert("Error getting photo: " + error);
-		});
+function photoWithoutZero() {
+	// Handle any errors
+	storageRef.child(parseInt(document.getElementById("ticketInput").value, 10) + ".jpg").getDownloadURL().then(function(url) {
+		// `url` is the download URL for 'images/stars.jpg'
+
+		// Or inserted into an <img> element:
+		console.log(url);
+		var img = document.getElementById('imageSpace');
+		img.src = url;
+		document.getElementById("ticketInput").value = "";
+	}).catch(function(error) {
+		// Handle any errors
+		alert("Error getting photo: " + error);
 	});
 }
 
