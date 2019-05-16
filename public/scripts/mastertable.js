b9a3c76f6bb37a74fdca62416b904ea8093eaba9
JacobKoala <42662002+JacobKoala@users.noreply.github.com>
2019-05-16 10:48:07 -0500

    Update mastertable.js

--- public/scripts/mastertable.js
+++ public/scripts/mastertable.js
@@ -95,7 +95,7 @@ function colorCode(value) {
 }
 
 function confirmLoad() {
-  if (confirm("Loading this table can only be done about 25 times per day (unconfirmed number). Are you sure you want to load the table now?")) {
+  if (confirm("Loading this table can only be done about 25 times per day (unconfirmed number). If the quota is exceeded, other, more critical, functions may stop working as well. Are you sure you want to load the table now?")) {
     generateTable();
   }
 }
